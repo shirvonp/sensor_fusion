c1a7026ac1e05c12fdd6e5c6f79967f78ce65c90
Shirvon Paul <72811959+shirvonp@users.noreply.github.com>
2022-08-24 15:24:54 -0400

    Add files via upload

---
+++ student/filter.py
@@ -0,0 +1,139 @@
+# ---------------------------------------------------------------------
+# Project "Track 3D-Objects Over Time"
+# Copyright (C) 2020, Dr. Antje Muntzinger / Dr. Andreas Haja.
+#
+# Purpose of this file : Kalman filter class
+#
+# You should have received a copy of the Udacity license together with this program.
+#
+# https://www.udacity.com/course/self-driving-car-engineer-nanodegree--nd013
+# ----------------------------------------------------------------------
+#
+
+# imports
+import numpy as np
+
+# add project directory to python path to enable relative imports
+import os
+import sys
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+import misc.params as params 
+
+class Filter:
+    '''Kalman filter class'''
+    def __init__(self):
+        pass
+
+    def F(self):
+        ############
+        # TODO Step 1: implement and return system matrix F
+        ############
+        dt = params.dt
+        F = np.identity(6)
+        F[0,3] = dt
+        F[1,4] = dt
+        F[2,5] = dt
+
+        return F
+        
+        ############
+        # END student code
+        ############ 
+
+    def Q(self):
+        ############
+        # TODO Step 1: implement and return process noise covariance Q
+        ############
+        dt = params.dt
+        q = params.q
+        
+        q1 = dt * q
+        q2 = ((dt**2)/2) * q 
+        q3 = ((dt**3)/3) * q
+        
+        Q = np.matrix([[q3,0,0,q2,0,0],
+                      [0,q3,0,0,q2,0],
+                       [0,0,q3,0,0,q2],
+                       [q2,0,0,q1,0,0],
+                       [0,q2,0,0,q1,0],
+                       [0,0,q2,0,0,q1]
+                      ])
+                    
+
+        return Q
+        
+        ############
+        # END student code
+        ############ 
+
+    def predict(self, track):
+        ############
+        # TODO Step 1: predict state x and estimation error covariance P to next timestep, save x and P in track
+        ############
+
+        F = self.F()
+        Q = self.Q()
+        P = track.P
+        
+        x =  F * track.x
+        P = F*P*F.transpose() + Q
+        
+        track.set_x(x)
+        track.set_P(P)
+        
+        ############
+        # END student code
+        ############ 
+
+    def update(self, track, meas):
+        ############
+        # TODO Step 1: update state x and covariance P with associated measurement, save x and P in track
+        ############
+        x = track.x        
+        H = meas.sensor.get_H(x)     
+        gamma = self.gamma(track, meas)
+        
+        S = self.S(track, meas, H)
+        P = track.P
+        K = P*H.transpose()*np.linalg.inv(S)
+        x = x + K*gamma
+        I = np.identity(params.dim_state)
+        P = (I - K*H) * P
+        
+        track.set_x(x)
+        track.set_P(P)
+        
+        ############
+        # END student code
+        ############ 
+        track.update_attributes(meas)
+    
+    def gamma(self, track, meas):
+        ############
+        # TODO Step 1: calculate and return residual gamma
+        ############
+
+        H = meas.sensor.get_hx(track.x)
+        
+        gamma = meas.z - H
+        
+        return gamma
+        
+        ############
+        # END student code
+        ############ 
+
+    def S(self, track, meas, H):
+        ############
+        # TODO Step 1: calculate and return covariance of residual S
+        ############
+
+        S = H*track.P*H.transpose() + meas.R
+
+        return S
+        
+        ############
+        # END student code
+        ############ 
